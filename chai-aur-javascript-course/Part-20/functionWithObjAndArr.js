0c373ca134eaba3e4155cbcb0b9a8c6e298aa481
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2023-12-03 10:58:10 +0530

    chai-aur-javascript part-20, 21

--- chai-aur-javascript-course/Part-20/functionWithObjAndArr.js
+++ chai-aur-javascript-course/Part-20/functionWithObjAndArr.js
@@ -2,32 +2,31 @@
 console.log("***".repeat(3), "REST OPERATOR", "***".repeat(3));
 
 const calculateCartPrice = (userName, category, ...price) => {
-    let sum = null
-    for(let i of price){
-        sum = sum + i
-    }
-    console.log(`${userName} baught ${sum} amount of products from ${category} category`);
-}
-calculateCartPrice("pranjal gogoi", "grocery", 200, 300, 400, 500, 100)
-
+  let sum = null;
+  for (let i of price) {
+    sum = sum + i;
+  }
+  console.log(
+    `${userName} baught ${sum} amount of products from ${category} category`
+  );
+};
+calculateCartPrice("pranjal gogoi", "grocery", 200, 300, 400, 500, 100);
 
 // # FUNCTION WITH OBJECT
 console.log("***".repeat(3), "FUNCTION WITH OBJECT", "***".repeat(3));
 
 const userObj = {
-    name : "pranjal gogoi",
-    age : 24
-}
+  name: "pranjal gogoi",
+  age: 24,
+};
 
-
-const myFunc = (anyObj) => {
-    console.log(anyObj.name, anyObj.age);
-}
-myFunc(userObj)
-
+const myFunc = anyObj => {
+  console.log(anyObj.name, anyObj.age);
+};
+myFunc(userObj);
 
 // destructuring the object
-const func = ({name, age}) => {
-    console.log(name, age);
-}
-func(userObj)
+const func = ({ name, age }) => {
+  console.log(name, age);
+};
+func(userObj);
--- chai-aur-javascript-course/Part-21/scopes.js
+++ chai-aur-javascript-course/Part-21/scopes.js
@@ -1,16 +1,18 @@
-let a = 1
-const b = 2
-var c = 3
-let name = "pranjal gogoi"
+let a = 1;
+const b = 2;
+var c = 3;
+let name = "pranjal gogoi";
 
-if(true){
-    let a = 11  // let is a block scope
-    const b = 22  // const is a block scope
-    var c = 33
-    name = "krishna yadav"  // updation is possible
+if (true) {
+  let a = 11; // let is a block scope
+  const b = 22; // const is a block scope
+  var c = 33;
+  name = "krishna yadav"; // updation is possible
 }
 
 console.log(a);
 console.log(b);
 console.log(c);
 console.log(name);
+
+// GLOBAL SCOPE IN CONSOLE IS DIFFERENT AND IN NODE ENVIRONMENT IS DIFFERENT
